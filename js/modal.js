72ebb975ba99c626c2f53ff5b6a2d527845c6032
Skiff90 <skiffvlad90@gmail.com>
2022-10-31 00:31:59 +0200

    commit test 1

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 (() => {
   const refs = {
@@ -12,4 +13,37 @@
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
+=======
+<<<<<<< HEAD
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+=======
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+>>>>>>> 560162439b02b58c8fa1320ab641e203e0b0901c
+>>>>>>> Stashed changes
 })();
